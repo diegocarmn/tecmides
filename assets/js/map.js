05d16308d266ecea424aa503660c90e15c7f3105
Diego Carmona <diegoncarmona@gmail.com>
2025-06-03 09:13:13 -0300

    API key injection changes

---
+++ assets/js/map.js
@@ -0,0 +1,20 @@
+const loadMapScript = () => {
+    const script = document.createElement('script');
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAcGIGliBnY4y0H_6mCRlUhPMOSTbo1yHQ&callback=initMap";
+    script.async = true;
+    document.head.appendChild(script);
+};
+
+function initMap() {
+        var uluru = {lat: -30.0331537, lng: -51.2192327};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      };
+      
+loadMapScript();
